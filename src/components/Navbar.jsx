306265f9bfe3eb8e239790784558a8e3ce24627a
julio <julioroja987@gmail.com>
2022-10-31 20:21:59 -0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import logo from "../assets/images/logo.svg";
+
+const Navbar = () => {
+    return (
+        <>
+            {/* Navbar */}
+            <nav className="relative container mx-auto p-6">
+                {/* Flex container */}
+                <div className="flex items-center justify-between">
+                    {/* Logo */}
+                    <div className="pt-2">
+                        <img src={logo} alt="logo de la compañia" />
+                    </div>
+                    {/* Menu items */}
+                    <div className="pt-2">
+                        <div className="hidden md:flex space-x-6">
+                            <a href="#" className="hover:text-darkGrayishBlue">
+                                Pricing
+                            </a>
+                            <a href="#" className="hover:text-darkGrayishBlue">
+                                Products
+                            </a>
+                            <a href="#" className="hover:text-darkGrayishBlue">
+                                About us
+                            </a>
+                            <a href="#" className="hover:text-darkGrayishBlue">
+                                Careers
+                            </a>
+                            <a href="#" className="hover:text-darkGrayishBlue">
+                                Community
+                            </a>
+                        </div>
+                    </div>
+                    {/* Button */}
+                    <a
+                        href=""
+                        className="hidden md:block p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight"
+                    >
+                        Get Started
+                    </a>
+                </div>
+            </nav>
+        </>
+    );
+};
+
+export default Navbar;
